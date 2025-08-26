10f28444896e4b6dc0770641d568e346b25f8800
nando <castill0jcz@hotmail.com>
2025-08-25 20:46:33 -0700

    back to the pc

--- breakout-clone/Scripts/ball.gd
+++ breakout-clone/Scripts/ball.gd
@@ -1,3 +1,10 @@
-extends Node2D
+extends RigidBody2D
 
 const SPEED :int  = 300
+
+func _ready() -> void:
+	
+	linear_velocity = Vector2(SPEED, -SPEED)
+
+func _physics_process(delta: float) -> void:
+	print(linear_velocity)
